4d9104823fbfda0c071ba2131009f979ab96d5fd
MEtooHARD <polar940301@gmail.com>
2024-11-10 22:09:44 +0800

    t

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   output: 'export',
-  distDir: 'out',
-  basePath: '/ShotGunChamber',
-  assetPrefix: '/ShotGunChamber/'
+  // distDir: 'out',
+  assetPrefix: isProd ? '/ShotGunChamber/' : '',
+  basePath: isProd ? '/ShotGunChamber' : ''
+  // basePath: '/ShotGunChamber',
+  // assetPrefix: '/ShotGunChamber/'
 };
 
 export default nextConfig;
