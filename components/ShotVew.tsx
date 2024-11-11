864456bb556ff7658bd3918e61b539cfb9f61a8c
benjounoou <126790799+benjounoou@users.noreply.github.com>
2024-11-12 01:45:06 +0800

    gg

--- components/ShotVew.tsx
+++ components/ShotVew.tsx
@@ -11,9 +11,9 @@ type props = {
 }
 
 const typeColor = {
-    [Shot.Type.Unknown]: 'bg-[#969696]',
-    [Shot.Type.Live]: 'bg-[#cf0000]',
-    [Shot.Type.Blank]: 'bg-[#00eeff]'
+    [Shot.Type.Unknown]: 'bg-gradient-to-br from-45% from-gray-600 to-gray-900',
+    [Shot.Type.Live]: 'bg-gradient-to-br from-45% from-red-600 to-red-900',
+    [Shot.Type.Blank]: 'bg-gradient-to-br from-45% from-blue-600 to-blue-900'
 }
 
 const nextType = {
