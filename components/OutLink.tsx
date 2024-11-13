121854f9902c5bcf26013c1499a0e8dc360dea16
MEtooHARD <polar940301@gmail.com>
2024-11-14 03:21:10 +0800

    dark mode alpha

---
+++ components/OutLink.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+
+export function OutLink({ children, url, classOverr = '' }: { children: ReactNode, url: string, classOverr?: string }) {
+    return <a className={"text-gray-500 dark:text-gray-400" + ` ${classOverr}`} href={url}>{children}</a>
+}
+
+export function Title({ children }: { children: ReactNode }) {
+    return <p className="text-gray-400 dark:text-gray-600 font-bold text-3xl select-none">{children}</p>
+}
