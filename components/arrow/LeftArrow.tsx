2dd3da918421a6fc078c4a3368db206e96b1995e
MEtooHARD <polar940301@gmail.com>
2024-11-14 23:21:09 +0800

    fixed arrows not toggling color

--- components/arrow/LeftArrow.tsx
+++ components/arrow/LeftArrow.tsx
@@ -15,6 +15,6 @@ export function LeftArrow({
         className="h-14 aspect-square cursor-pointer transform duration-75 hover:scale-110 active:scale-95"
         onClick={() => { setRound(prev => loop(prev - 1, sum)) }}
         viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <polygon fillRule="evenodd" fill={"#ededed"} points="9.414 12 16.707 19.293 15.293 20.707 6.586 12 15.293 3.293 16.707 4.707" />
+        <polygon className="dark:fill-[#ededed]" fillRule="evenodd" fill={"#000000"} points="9.414 12 16.707 19.293 15.293 20.707 6.586 12 15.293 3.293 16.707 4.707" />
     </svg>
 }
